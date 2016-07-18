7f3963bbc409d7978e666885e5a09330d9d91619
Susana C Delgadillo <susana@chartbeat.com>
2016-07-18 13:35:00 -0400

    refactor and added more event listeners - refactored to use self invoking func (more important in the content script to keep global namespace clean and avoid collisions) - added more listeners to store and reflect stored options in popup script - added listeners in content script to actually read options from storage

--- pull_request_files.js
+++ pull_request_files.js
@@ -1,9 +1,48 @@
-file_headers = document.getElementsByClassName("file-header")
-for (var i = 0; i < file_headers.length; i++) {
-  file_header = file_headers[i];
-  file_header.addEventListener("click", function() {
-    // data = this.nextElementSibling
-    data = this.parentElement.getElementsByClassName("data")[0]
-    data.classList.toggle("hidden");
-  }, false);
-}
+(function(window, document) {
+
+  var featureEvents = {
+    "ghp_enable_collapsible_files": enableCollapsibleFiles,
+    "ghp_enable_sticky_file_list": null,
+    "ghp_enable_perma_expanded_file_list": null,
+    "ghp_disable_whitespace_diffs": null,
+  };
+
+  function init() {
+    createMenuListeners();
+  }
+
+  function createMenuListeners () {
+    var features = featureEvents.keys;
+    chrome.storage.sync.get(features, function(items) {
+      console.log(items);
+      for (var item in items) {
+        if (item in featureEvents && items[item] === true && featureEvents[item] !== null) {
+          fn = featureEvents[item];
+          fn();
+        } else {
+          console.log(item, "not used.");
+        }
+      }
+    });
+  }
+
+  function enableCollapsibleFiles() {
+    var files = document.getElementById("files").getElementsByClassName("file");
+    var numFiles = files.length;
+    for (var i = 0; i < numFiles; i++) {
+      var file = files[i];
+      makeFileCollapsible(file)
+    }
+  }
+
+  function makeFileCollapsible(fileEl) {
+    var fileHeader = fileEl.getElementsByClassName("file-header")[0];
+    fileHeader.addEventListener("click", function() {
+      var container = this.parentElement
+      var data = container.getElementsByClassName("data")[0]
+      data.classList.toggle("hidden");
+    }, false);
+  }
+
+  init();
+})(window, document);
